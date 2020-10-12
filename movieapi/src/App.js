47d43983fc3880cba4191bb41f2798fe5bde1bd0
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-10-12 15:53:12 -0500

    initialized my project

---
+++ movieapi/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
