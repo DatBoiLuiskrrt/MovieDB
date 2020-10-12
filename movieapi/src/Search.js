4e9e6c966abe42419410add8b869c3e688dba68d
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-10-12 18:37:52 -0500

    created a function that fetches the api

--- movieapi/src/Search.js
+++ movieapi/src/Search.js
@@ -1,8 +1,27 @@
 import React from "react";
 
 export default function Search() {
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+
+        const query = "Jurassic Park";
+
+        const url = `https://api.themoviedb.org/3/movie/550?api_key=7ef6152a444c38c3338ad6ca46aa51c2&language=en-US&query=${query}page=1&include_adult=false`;
+
+        try{
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log("error");
+    }
+}
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">
                 Movie name
             </label>
