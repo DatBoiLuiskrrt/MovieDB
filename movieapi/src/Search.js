3ee8d56987f3bd67246f3ecec6a7076ae704988d
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-10-12 21:19:38 -0500

    fixed the problem with the api

--- movieapi/src/Search.js
+++ movieapi/src/Search.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Search() {
+    const [query, setQuery] = useState('');
 
     const searchMovies = async (e) => {
         e.preventDefault();
         console.log("submitting");
 
-        const query = "Jurassic Park";
+        
 
-        const url = `https://api.themoviedb.org/3/movie/550?api_key=7ef6152a444c38c3338ad6ca46aa51c2&language=en-US&query=${query}page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=7ef6152a444c38c3338ad6ca46aa51c2&language=en-US&query=${query}&page=1&include_adult=false`;
+        
 
         try{
         const res = await fetch(url);
@@ -28,7 +30,9 @@ export default function Search() {
                 <input 
                  type="text"
                  className="input" 
-                 placeholder="Search">
+                 placeholder="Search"
+                 value={query}
+                 onChange={ (e) => setQuery(e.target.value)}>
                 </input>
                 <button className="button" type="submit">Enter</button>
             
